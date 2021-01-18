e76f16747c54d119aa72a0e7047d824141fcb560
Russ <3870824+pocketfulofrage@users.noreply.github.com>
2021-01-17 23:52:25 -0500

    Update larbs.sh

--- larbs.sh
+++ larbs.sh
@@ -14,8 +14,8 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
-# [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/pocketfulofrage/dotfiles.git"
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/pocketfulofrage/dotfiles.git"
+# [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
 [ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/pocketfulofrage/larbs/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
